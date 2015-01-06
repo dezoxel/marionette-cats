959e648d7899306fbb3b59cf6ec79379d81ea651
dezoxel <dezoxel@gmail.com>
2015-01-06 11:15:00 +0300

    Implemented cat item view skeleton

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -9,3 +9,9 @@ var AngryCat = Backbone.Model.extend({});
 var AngryCats = Backbone.Collection.extend({
   model: AngryCat
 });
+
+var AngryCatView = Backbone.Marionette.ItemView.extend({
+  template: '#angry_cat-template',
+  tagName: 'tr',
+  className: 'angry_cat'
+});
